8900db4ff9f2d0ec3e63f00f3f7fecc50ce3a1e0
mkevin795-web <mkevin795@gmail.com>
2026-04-21 22:48:03 -0400

    inventario-pwa-1.5

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+const CACHE_NAME = "inventario-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
