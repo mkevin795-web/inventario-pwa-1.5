27945674f92db4e1d03c31d3f818ca3c845339a1
mkevin795-web <mkevin795@gmail.com>
2026-04-22 10:31:18 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
-
-const CACHE_NAME = "inventario-v1";
+const CACHE_NAME = "inventario-v5";
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
     })
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
